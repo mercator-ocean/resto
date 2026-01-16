f14b34239cdf9c604a39f3d311a725886b16a3bc
Bitard Michaël <bitard.michael@gmail.com>
2026-01-16 14:48:24 +0100

    first simple integration test

---
+++ tests/integration/UsersTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace test;
+
+use PHPUnit\Framework\TestCase;
+
+function httpPost($url, $data)
+{
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Content-Length: ' . strlen($data)
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
+
+function user($username, $email)
+{
+    return array(
+        "username" => $username,
+        "firstname" => "John",
+        "lastname" => "Doe",
+        "email" => $email,
+        "password" => "dummy"
+      );
+}
+
+final class UsersTest extends TestCase
+{
+    public function testCanCreateUser(): void
+    {
+        $response = httpPost("http://localhost:5252/users", json_encode(user("newuser2", "newUser2@toto.fr")));
+
+        $decoded = json_decode($response);
+        $this->assertSame($decoded->status, "success");
+    }
+}
