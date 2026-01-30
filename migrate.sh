496c3dc1878b88737d57d4bed67b2910cd0e1ac1
Bitard Michaël <bitard.michael@gmail.com>
2026-01-30 14:10:44 +0100

    end migration script

--- migrate.sh
+++ migrate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 psql -U resto -h 127.0.0.1 <<SQL
-DROP SCHEMA resto;
+DROP SCHEMA resto CASCADE;
 CREATE SCHEMA IF NOT EXISTS resto;
 CREATE EXTENSION IF NOT EXISTS ltree WITH SCHEMA resto;
 SQL
@@ -15,5 +15,9 @@ ALTER TABLE resto.group ADD CONSTRAINT idx_name_unique UNIQUE (name);
 ALTER TABLE resto.catalog ADD COLUMN pinned BOOLEAN;
 --- End Migration
 
+UPDATE resto.catalog_feature SET catalogid = 'project/'||catalogid WHERE catalogid IN (SELECT id FROM resto."catalog" c WHERE c.rtype IS NULL OR c.rtype = 'catalog' AND c.id NOT LIKE 'variable_families/%');
+UPDATE resto.catalog_feature SET path=text2ltree(replace(catalogid, '/', '.'));
+
 UPDATE resto.catalog SET id='project/'||id WHERE rtype IS NULL OR rtype = 'catalog' AND id NOT LIKE 'variable_families/%'; 
+
 SQL
