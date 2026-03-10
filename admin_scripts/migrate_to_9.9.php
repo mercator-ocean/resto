e807a8ef99acfb506ffde94f4e2fffd91ead3f90
Chloé Delpont-Ramat <cdelpontramat@mercator-ocean.fr>
2026-03-10 14:22:42 +0100

    Change the default value of user information display to false

---
+++ admin_scripts/migrate_to_9.9.php
@@ -0,0 +1,30 @@
+#!/command/with-contenv php
+<?php
+
+require_once("/app/resto/core/RestoConstants.php");
+require_once("/app/resto/core/RestoDatabaseDriver.php");
+require_once("/app/resto/core/utils/RestoLogUtil.php");
+require_once("/app/resto/core/dbfunctions/UsersFunctions.php");
+
+/*
+     * Read configuration from file...
+     */
+$configFile = '/etc/resto/config.php';
+if (!file_exists($configFile)) {
+    exit(1);
+}
+$config = include($configFile);
+$dbDriver = new RestoDatabaseDriver($config['database'] ?? null);
+$queries = [];
+try {
+    $dbDriver->query('BEGIN');
+
+    /* User information is now false by default */
+    $dbDriver->query('ALTER TABLE ' . $dbDriver->commonSchema . '.user ALTER COLUMN settings SET DEFAULT \'{"showBio":false,"showIdentity":false,"showTopics":false,"showEmail":false}\'');
+
+    $dbDriver->query('COMMIT');
+} catch (Exception $e) {
+    $dbDriver->query('ROLLBACK');
+    RestoLogUtil::httpError(500, $e->getMessage());
+}
+echo "Looks good\n";
